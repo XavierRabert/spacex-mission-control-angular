95548162d0da06a893d5d2a630be4b13f47258da
Xavier <xrabert@gmail.com>
2026-02-16 13:26:13 +0100

    feat: add app shell layout with navbar and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Shell } from './core/layout/shell/shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@layout/shell/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'launches',
+      },
+
+      {
+        path: 'launches',
+        loadComponent: () =>
+          import('@features/launches/launches-page/launches-page').then((m) => m.LaunchesPage),
+      },
+
+      {
+        path: 'rockets',
+        loadComponent: () =>
+          import('@features/rockets/rockets-page/rockets-page').then((m) => m.RocketsPage),
+      },
+
+      {
+        path: 'favorites',
+        loadComponent: () =>
+          import('@features/favorites/favorites-page/favorites-page').then((m) => m.FavoritesPage),
+      },
+    ],
+  },
+
+  // Optional: 404 fallback
+  {
+    path: '**',
+    redirectTo: 'launches',
+  },
+];
